a964884682f19f27056579df7cfe26fd7549e41a
mohamm-alsaid <mohamm.alsaid@gmail.com>
2020-06-30 07:15:45 -0700

    add rest of unit specs for time

--- DERAS-Server/src/tests/TimeTest.cpp
+++ DERAS-Server/src/tests/TimeTest.cpp
@@ -31,9 +31,57 @@ namespace TimeTest{
         t1.currentTime<t3? cout<<"PASS\n":cout<<"FAIL\n";
         return;
     }
-    void Dest_Time_Test()
+    void Dst_Time_Test()
     {
+        Time t;
+        time_t tmStart = t.dstStartTime;
+        time_t tmEnd = t.dstEndTime;
 
+
+        cout<<t.dstStartTime<<" to --> "<<t.dstEndTime<<endl;
+        // calculate start
+        tm* localStart = localtime(&tmStart);
+        // just to make it human readable only
+        stringstream st,en;
+        st<<std::put_time(localStart,"%c\n");
+        cout<<st.str()<<endl; // ISSUE HERE ---- should be [March 8th - March 14th] (second Sunday in March)
+        cout<<"Week day ( Sun = 0 & Sat = 6): "<<localStart->tm_wday<<endl;
+        cout<<"Month: "<<localStart->tm_mon+1<<endl<<endl<<endl;;
+
+        // calculate end
+        tm* localEnd = localtime(& tmEnd);
+        en<<std::put_time(localEnd, "%c\n");
+        cout<<en.str()<<endl; // ISSUE HERE ---- should be [Nov 1 th - Nov 7th] (first Sunday in November)
+        cout<<"Week day ( Sun = 0 & Sat = 6): "<<localEnd->tm_wday<<endl;
+        cout<<"Month: "<<localEnd->tm_mon + 1<<endl;
+
+        // Might be off-by-one error in boost?
+
+    }
+    void Quality_Test()
+    {
+        Time t;
+        cout<<"QUALITY: "<<+t.quality<<endl;
+        cout<<"TEST QUALITY: "<<"3 <= "<<+t.quality<<" <= 7"<<endl;
+        t.quality >= 3 && t.quality<=7? cout<<"PASS\n":cout<<"FAIL\n";
+    }
+    void Href_Test()
+    {
+        Time t;
+        cout<<"TEST HREF: "<<t.href<<" !=  EMPTY"<<endl;
+        !t.href.empty()? cout<<"PASS\n":cout<<"FAIL\n";
+    }
+    void XMLNS_Test()
+    {
+        Time t;
+        cout<<"TEST XMLNS: "<<t.xmlns<<" !=  EMPTY"<<endl;
+        !t.xmlns.empty()? cout<<"PASS\n":cout<<"FAIL\n";
+    }
+    void Poll_Rate_Test()
+    {
+        Time t;
+        cout<<"TEST POLL RATE: "<<t.pollRate<<" >= "<< 0<<endl;
+        t.pollRate>=0? cout<<"PASS\n":cout<<"FAIL\n";
     }
     void Local_Time_Test()
     {
@@ -41,12 +89,11 @@ namespace TimeTest{
         long int cur_tm = (long int) t1.currentTime;
         time_t current1 = time(&cur_tm);
         time_t current2 = time(0);
-
         tm* local1 = localtime(&current1);
         tm* local2 = localtime(&current2);
         string l1 = to_string(local1->tm_hour)+":"+to_string(local1->tm_min)+":"+to_string(local1->tm_sec);
         string l2 = to_string(local2->tm_hour)+":"+to_string(local2->tm_min)+":"+to_string(local2->tm_sec);
-
+        // testing for the formatting as the local time is calculated using the current time (which run prior to this)
         cout<<"TEST LOCAL TIME (FORMATTED): "<<l1<<" == "<< l2<<endl;
         l1==l2? cout<<"PASS\n":cout<<"FAIL\n";
 
@@ -76,7 +123,7 @@ namespace TimeTest{
     }
     int main()
     {
-        cout<<" ---------------------- Time Test ---------------------- "<<endl;
+        cout<<" ---------------------- Time Test Start --------------------- "<<endl;
         cout<<"\n----->testing current time...\n\n";
         Current_Time_Test();
         cout<<"----------------------------------\n";
@@ -86,6 +133,21 @@ namespace TimeTest{
         cout<<"\n----->testing local time...\n\n";
         Local_Time_Test();
         cout<<"----------------------------------\n";
+        cout<<"\n----->testing Daylight Saving Start Time...\n\n";
+        Dst_Time_Test();
+        cout<<"----------------------------------\n";
+        cout<<"\n----->testing Poll Rate...\n\n";
+        Poll_Rate_Test();
+        cout<<"----------------------------------\n";
+        cout<<"\n----->testing XML Namespace Time...\n\n";
+        XMLNS_Test();
+        cout<<"----------------------------------\n";
+        cout<<"\n----->testing Href...\n\n";
+        Href_Test();
+        cout<<"----------------------------------\n";
+        cout<<"\n----->testing Quality...\n\n";
+        Quality_Test();
+        cout<<" ---------------------- Time Test End ---------------------- "<<endl;
         return 0;
     }
 }
